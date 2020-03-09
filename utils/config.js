d0787e5215af9162a0f676787e27a189203344e4
Max Du <max.du@farfetch.com>
2020-03-09 13:07:03 +0800

    WIP

--- utils/config.js
+++ utils/config.js
@@ -14,7 +14,7 @@
 //配置域名,域名只修改此处。
 //如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
 var DOMAIN = "ezainz.chcbst.com"
-var WEBSITENAME="易在纽国"; //网站名称
+var WEBSITENAME="比洋价(易在纽国)"; //网站名称
 var PAGECOUNT='10'; //每页文章数目
 var PAYTEMPPLATEID = 'hzKpxuPF2rw7O-qTElkeoE0lMwr0O4t9PJkLyt6v8rk';//鼓励消息模版id
 var REPLAYTEMPPLATEID = 'IiAVoBWP34u1uwt801rI_Crgen7Xl2lvAGP67ofJLo8';//回复评论消息模版id
